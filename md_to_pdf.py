674d0d5c2e0693609e9dcc136b2929b63820d829
byronlove-lace <byronlovelace@proton.me>
2023-08-13 17:36:10 +0700

    see previous entry

--- md_to_pdf.py
+++ md_to_pdf.py
@@ -9,9 +9,17 @@ into simple pdfs
 it's designed to be run entirely from the command line
 '''
 
+'''
 subprocess.run('touch pdf_blueprint.md', shell=True)
 subprocess.run("pdf_blueprint.md < 'Enter text:'", shell=True)
 subprocess.run('nvim +3 pdf_blueprint.md', shell=True)
+'''
+css_file = Path('pdf_formatting.css')
 
+css_file.exists()
+css_file.is_dir()
+# with css_file.open('rb') as f:
+#     css = f.read()
 
 markdown.markdownFromFile(input='pdf_blueprint.md', output='pdf_blueprint.html')
+pdfkit.from_file(input='pdf_blueprint.html', output_path='output.pdf')
